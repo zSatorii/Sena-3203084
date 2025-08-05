4d5e60474098cad183eac4b5418a81701745644a
Yuuki <johanikaros@gmail.com>
2025-08-04 21:40:23 -0500

    .

---
+++ Modulo2/Modulo2.10.py
@@ -0,0 +1,5 @@
+contador_pares = 0
+for i in range(1, 21):
+    if i % 2 == 0:
+        contador_pares += 1
+print("Hay", contador_pares, "números pares en el rango de 1 a 20.")
---
+++ Modulo2/Modulo2.8.py
@@ -0,0 +1,5 @@
+numeros = [1, 2, 3, 4, 5]
+suma = 0
+for numero in numeros:
+    suma += numero
+print("La suma de los números es:", suma)
