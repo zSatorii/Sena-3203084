553a5e301bd5a0154755a081c54e6dbb5a97316f
Yuuki <64563861+zSatorii@users.noreply.github.com>
2025-08-08 22:09:03 +0000

    Se corrige la lista para que quede bien

--- Trabajo en clase/8/08/25/ListaCalculadoraIf.py
+++ Trabajo en clase/8/08/25/ListaCalculadoraIf.py
@@ -1,17 +1,21 @@
-r = (input("Seleccione el tipo de operación (+, -, *, /) "))
-n1 = int(input("Ingrese el primer número: "))
-n2 = int(input("Ingrese el segundo número: "))
 
-lista=[r,n1,n2]
+lista=["suma", "resta", "multiplicar", "dividir"]
+ingresar = (int(input("ingrese el numero (1=suma 2=resta 3=multiplicar 4=dividir ")))
+n1 =(int(input("ingrese el primer numero ")))
+n2 =(int(input("ingrese el segundo numero ")))
 
-if lista[r] == "+":
-    print(lista[1]+lista[2])
+if ingresar == 1:
+    print(lista[0])
+    print(n1+n2)
 else:
-    if lista[r] == "-":
-        print(lista[1]-lista[2])
+    if ingresar == 2:
+        print(lista[1])
+        print(n1-n2)
     else:
-        if lista[r] == "*":
-            print(lista[1]*lista[2])
+        if ingresar == 3:
+            print(lista[2])
+            print(n1*n2)
         else:
-            if lista[r] == "/":
-                print(lista[1]/lista[2])
+            if ingresar == 4:
+                print(lista[3])
+                print(n1/n2)
